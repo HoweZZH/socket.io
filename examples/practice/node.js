3a6f446a421b81be8b5fbc3216f2f72bc2574ce8
Zihao Zhao <zhaozihao7199@gmail.com>
2016-07-18 21:27:13 -0400

    add

---
+++ examples/practice/node.js
@@ -0,0 +1,9 @@
+
+var Note=React.createClass({
+    render:function(){
+        return (
+        <div className='note'> </div>
+        );
+    }
+});
+ReactDOM.render(<Note>Hello world!!S2223</Note>,document.getElementById('react-container'));
